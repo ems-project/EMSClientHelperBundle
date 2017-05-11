eec3ffc19f56cce93aca44d1129d504a66adf9a5
Isabelle Michiels <isabelle.michiels@smals.be>
2017-05-11 09:58:35 +0200

    first commit

---
+++ Controller/DefaultController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace EMS\ClientHelperBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\HttpFoundation\File\File;
+
+class DefaultController extends Controller
+{   
+
+    /**
+     * @Route("/templates", name="templates")
+     */
+    public function pageAction(Request $request) {
+    	
+    	$templates = $this->container->getParameter('ems_client_helper.templates');
+    	
+    	$twigs = array();
+
+    	foreach ($templates as $key =>$template) {
+    		$finder = new Finder();
+    		$finder->files()->name('*.twig')->in($this->get('kernel')->locateResource('@' . $template['resource']) . $template['base_path']);
+    		
+    		foreach ($finder as $file) {
+    			/** @var File $file */
+    			$search  = array('\\', '.html.twig');
+    			$replace = array('/', '');
+    			$subject = $file->getRelativePathname();
+    			$twigs[$template['resource']][$template['base_path']][$file->getBasename()] = str_replace($search, $replace, $subject);
+    		}
+    	}
+    	
+    	if($this->container->getParameter('ems_client_helper.app_enabled')){
+    		$base_paths = $this->container->getParameter('ems_client_helper.app_base_path');
+    		
+    		foreach ($base_paths as $key =>$base_path) {
+    			$finder = new Finder();
+    			$finder->files()->name('*.twig')->in($this->get('kernel')->getRootDir() .'/' . $base_path);
+    		
+    			foreach ($finder as $file) {
+    				/** @var File $file */
+    				$search  = array('\\', '.html.twig');
+    				$replace = array('/', '');
+    				$subject = $file->getRelativePathname();
+    				$twigs['app'][$base_path][$file->getBasename()] = str_replace($search, $replace, $subject);
+    			}
+    		}
+    		 
+    	}
+    	
+    	return $this->render('EMSClientHelperBundle:Default:templates.html.twig',[
+    			'templates' => $twigs
+    	]);
+    }
+    
+    /**
+     * @Route("/templates/{template_path}", name="template", requirements={"template_path"=".+"})
+     */
+    public function templateAction($template_path, Request $request) {
+    
+    	$resource = $request->query->get('resource');
+   		$base_path = $request->query->get('base_path');
+   		
+   		$pos = strpos($base_path, '/views');
+   		
+   		$folder = '';
+   		if(strlen($base_path) > $pos + 7) {
+   			$folder = substr($base_path, $pos + 7);
+   		}
+   		
+   		if ($resource === 'app') {
+   			$path = $resource . '/' . $base_path;
+   			$render = $folder . '/' .  $template_path . '.html.twig';
+   		} else {
+   			$path= $this->get('kernel')->locateResource('@' . $resource) . $base_path;
+   			$render = $resource . ':' . $folder . ':' . $template_path . '.html.twig';
+   		}
+   		
+   		return $this->render($render ,[
+   				'template' => $template_path,
+   				'path' => $path . '/' . $template_path . '.html.twig',
+   		]);
+    }
+}
---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace EMS\ClientHelperBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files.
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/configuration.html}
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('ems_client_helper');
+
+        // Here you should define the parameters that are allowed to
+        // configure your bundle. See the documentation linked above for
+        // more information on that topic.
+        
+        $rootNode
+         ->children()
+         	->arrayNode('templates')
+   			  ->prototype('array')
+   			  	->cannotBeEmpty()
+             	->children()
+              	  ->scalarNode('resource')->cannotBeEmpty()->end()
+              	  ->scalarNode('base_path')->cannotBeEmpty()->end() 
+        	  ->end()
+        	->end()
+        	->defaultValue($this->getTemplatesDefaultValues())
+          ->end()
+          ->booleanNode('app_enabled')->defaultFalse()->end()
+          ->arrayNode('app_base_path')->prototype('scalar')->end() 
+        ->end();
+        
+
+        return $treeBuilder;
+    }
+    
+    /**
+     * Returns default values for profile menu (to avoid BC Break)
+     *
+     * @return array
+     */
+    protected function getTemplatesDefaultValues()
+    {
+    	return array(
+    			array(
+    					'resource'  => 'EMSClientHelperBundle',
+    					'base_path'  => 'Resources/views/Pages',
+    			),
+    	);
+    }
+}
