cce77173b177ddc269c61f64566bfab279c2333c
theus deka <me@theus.be>
2018-07-24 08:38:53 +0200

    At least 5.6 and ca bundle 1.1

--- EMSBackendBridgeBundle/Controller/ApiController.php
+++ EMSBackendBridgeBundle/Controller/ApiController.php
@@ -19,7 +19,7 @@ class ApiController extends AbstractController
      *
      * @return JsonResponse
      */
-    public function contentTypesAction($apiName)
+    public function contentTypesAction(ApiService $apiService, $apiName)
     {
         return $this->getApi()->getContentTypes($apiName)->getResponse();
     }
