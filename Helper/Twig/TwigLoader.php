5882b1fd980d4db3675cd4d49364289329edc96e
David Mattei <david.mattei@smals.be>
2018-12-10 16:46:29 +0100

    hotfix: template cache invalidation
    First look at _published_datetime then fallback to
    _finalization_datetime

--- Helper/Twig/TwigLoader.php
+++ Helper/Twig/TwigLoader.php
@@ -140,10 +140,10 @@ class TwigLoader implements \Twig_LoaderInterface
 
             $source = $document['_source'];
 
-            if(isset($source['_finalization_datetime'])) {
-                $date = new \DateTime($source['_finalization_datetime']);
-            } else if(isset($source['_published_datetime'])) {
+            if(isset($source['_published_datetime'])) {
                 $date = new \DateTime($source['_published_datetime']);
+            } else if(isset($source['_finalization_datetime'])) {
+                $date = new \DateTime($source['_finalization_datetime']);
             } else {
                 $date = new \DateTime();
             }
