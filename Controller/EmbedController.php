c713535b228a6b279ad3e4f988fcf1d1177c2fcf
David Mattei <david.mattei@smals.be>
2019-03-07 17:04:42 +0100

    rename: embed controller aggs to args

--- Controller/EmbedController.php
+++ Controller/EmbedController.php
@@ -17,13 +17,13 @@ class EmbedController extends AbstractController
         $this->clientRequest = $manager->getDefault();
     }
 
-    public function renderHierarchyAction(string $template, string $parent, string $field, int $depth = null, array $sourceFields = [], array $aggs = []): Response
+    public function renderHierarchyAction(string $template, string $parent, string $field, int $depth = null, array $sourceFields = [], array $args = []): Response
     {
         $hierarchy = $this->clientRequest->getHierarchy($parent, $field, $depth, $sourceFields);
 
         return $this->render($template, [
             'translation_domain' => $this->clientRequest->getCacheKey(),
-            'aggs' => $aggs,
+            'args' => $args,
             'hierarchy' => $hierarchy,
         ]);
     }
--- Resources/doc/twig.md
+++ Resources/doc/twig.md
@@ -47,7 +47,7 @@ and a Symfony\Component\Finder\SplFileInfo object as value.
     'field': 'children',
     'depth': 5,
     'sourceFields': [],
-    'aggs': {'extra': 'test'}
+    'args': {'extra': 'test'}
 } )) }}
 ```
 Example menu.html.twig
