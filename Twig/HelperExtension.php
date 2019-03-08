c3eceb0006d795d8fb9b082bf0b7797f26c3a682
David Meert <david.meert@smals.be>
2019-03-08 17:11:57 +0100

    refactor: use twig namespaces
    https://symfony.com/blog/new-in-twig-namespaced-classes

--- Twig/HelperExtension.php
+++ Twig/HelperExtension.php
@@ -7,6 +7,7 @@ use EMS\ClientHelperBundle\Helper\Asset\ProcessHelper;
 use EMS\ClientHelperBundle\Helper\Elasticsearch\ClientRequestRuntime;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
+use Twig\TwigFunction;
 
 class HelperExtension extends AbstractExtension
 {
@@ -16,8 +17,8 @@ class HelperExtension extends AbstractExtension
     public function getFilters()
     {
         return [
-            new \Twig_SimpleFilter('emsch_routing', [RoutingRuntime::class, 'transform'], ['is_safe' => ['html']]),
-            new \Twig_SimpleFilter('emsch_data', [ClientRequestRuntime::class, 'data'], ['is_safe' => ['html']]),
+            new TwigFilter('emsch_routing', [RoutingRuntime::class, 'transform'], ['is_safe' => ['html']]),
+            new TwigFilter('emsch_data', [ClientRequestRuntime::class, 'data'], ['is_safe' => ['html']]),
             new TwigFilter('emsch_html_encode', [TextRuntime::class, 'html_encode'], ['is_safe' => ['html']]),
             new TwigFilter('emsch_anti_spam', [TextRuntime::class, 'html_encode_pii'], ['is_safe' => ['html']]),
         ];
@@ -29,12 +30,12 @@ class HelperExtension extends AbstractExtension
     public function getFunctions()
     {
         return [
-            new \Twig_SimpleFunction('emsch_admin_menu', [AdminMenuRuntime::class, 'showAdminMenu'], ['is_safe' => ['html']]),
-            new \Twig_SimpleFunction('emsch_route', [RoutingRuntime::class, 'createUrl']),
-            new \Twig_SimpleFunction('emsch_search', [ClientRequestRuntime::class, 'search']),
-            new \Twig_SimpleFunction('emsch_assets', [AssetHelperRuntime::class, 'assets']),
-            new \Twig_SimpleFunction('emsch_unzip', [AssetHelperRuntime::class, 'unzip']),
-            new \Twig_SimpleFunction('emsch_process_asset', [ProcessHelper::class, 'generate']),
+            new TwigFunction('emsch_admin_menu', [AdminMenuRuntime::class, 'showAdminMenu'], ['is_safe' => ['html']]),
+            new TwigFunction('emsch_route', [RoutingRuntime::class, 'createUrl']),
+            new TwigFunction('emsch_search', [ClientRequestRuntime::class, 'search']),
+            new TwigFunction('emsch_assets', [AssetHelperRuntime::class, 'assets']),
+            new TwigFunction('emsch_unzip', [AssetHelperRuntime::class, 'unzip']),
+            new TwigFunction('emsch_process_asset', [ProcessHelper::class, 'generate']),
         ];
     }
 }
