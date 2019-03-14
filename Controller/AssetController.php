8c66d0a32c2bdfc500652bc6de811755972a20cd
theus77 <me@theus.be>
2019-03-14 12:05:06 +0100

    S3-Support (#92)

--- Controller/AssetController.php
+++ Controller/AssetController.php
@@ -16,8 +16,18 @@ class AssetController
         $this->processHelper = $processHelper;
     }
 
+    /**
+     * @deprecated
+     * @param Request $request
+     * @param string $processor
+     * @param string $hash
+     * @param string|null $configHash
+     * @return Response
+     */
     public function process(Request $request, string $processor, string $hash, string $configHash = null): Response
     {
+        @trigger_error("AssetController::process is deprecated use the ems_asset twig filter to generate the route", E_USER_DEPRECATED);
+
         return $this->processHelper->process($request, $processor, $hash, $configHash);
     }
 }
