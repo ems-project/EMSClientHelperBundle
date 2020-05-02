5a75c5a0cdf52d152bbc498592d0bc09c1268744
theus deka <me@theus.be>
2020-05-02 20:59:22 +0200

    bug[session]: bind the locale in a cookie instead of in a session
    Until now the only reason why a session was started is to save the current locale. As a session save its ID in a cookie and as a session id is way more longer than a locale; let's save directly the locale in a cookie.

--- EventListener/KernelListener.php
+++ EventListener/KernelListener.php
@@ -9,8 +9,10 @@ use EMS\ClientHelperBundle\Helper\Environment\EnvironmentHelper;
 use EMS\ClientHelperBundle\Helper\Translation\TranslationHelper;
 use Symfony\Component\Debug\Exception\FlattenException;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\KernelEvents;
 
@@ -62,8 +64,10 @@ class KernelListener implements EventSubscriberInterface
         return [
             KernelEvents::REQUEST => [
                 ['bindEnvironment', 100],
-                ['bindLocale', 17],
                 ['loadTranslations', 11],
+            ],
+            KernelEvents::RESPONSE => [
+                ['bindLocale', 17],
             ],
             KernelEvents::EXCEPTION => [
                 ['bindEnvironment', 100],
@@ -100,15 +104,10 @@ class KernelListener implements EventSubscriberInterface
         }
     }
 
-    /**
-     * @param GetResponseEvent $event
-     */
-    public function bindLocale(GetResponseEvent $event)
+    public function bindLocale(ResponseEvent $event)
     {
-        $request = $event->getRequest();
-
-        if ($locale = $this->localeHelper->getLocale($request)) {
-            $request->getSession()->set('_locale', $locale);
+        if ($locale = $this->localeHelper->getLocale($event->getRequest())) {
+            $event->getResponse()->headers->setCookie(new Cookie('_locale', $locale, strtotime('now + 12 months')));
         }
     }
 
