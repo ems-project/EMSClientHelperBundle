b88c55060b89a490ae7b334be10ae57d40160b8a
David Mattei <david.mattei@smals.be>
2021-01-14 19:31:31 +0100

    refactor: Routing transform clientRequestManager
    The compiler should not inject the clientRequest, we can just use manager->getDefault(). In the feature the clientRequestManager will also be removed.
    
    About the ems_client_helper.routing.relative_paths, it is not configurable from our current repositories. We don't use the option anymore. So we removed it, because it was the last option in the routing section.

--- src/Helper/Routing/Url/Generator.php
+++ src/Helper/Routing/Url/Generator.php
@@ -4,15 +4,12 @@ declare(strict_types=1);
 
 namespace EMS\ClientHelperBundle\Helper\Routing\Url;
 
-use EMS\CommonBundle\Common\EMSLink;
 use Symfony\Component\Routing\RouterInterface;
 
 final class Generator
 {
     private string $baseUrl = '';
     private string $phpApp = '';
-    /** @var array<int, array{regex: string, path: string}> */
-    private array $relativePaths;
 
     /**
      * Regex for getting the base URL without the phpApp
@@ -20,10 +17,7 @@ final class Generator
      */
     private const REGEX_BASE_URL = '/^(?P<baseUrl>\/.*?)(?:(?P<phpApp>\/[\-_A-Za-z0-9]*.php)|\/|)$/i';
 
-    /**
-     * @param array<int, array{regex: string, path: string}> $relativePaths
-     */
-    public function __construct(RouterInterface $router, array $relativePaths = [])
+    public function __construct(RouterInterface $router)
     {
         \preg_match(self::REGEX_BASE_URL, $router->getContext()->getBaseUrl(), $match);
 
@@ -34,8 +28,6 @@ final class Generator
         if (isset($match['phpApp'])) {
             $this->phpApp = $match['phpApp'];
         }
-
-        $this->relativePaths = $relativePaths;
     }
 
     public function createUrl(string $relativePath, string $path): string
@@ -43,27 +35,15 @@ final class Generator
         return $this->baseUrl.$relativePath.$this->phpApp.$path;
     }
 
-    public function prependBaseUrl(EMSLink $emsLink, string $url): string
+    public function prependBaseUrl(string $url): string
     {
         $url = \trim($url);
-        $path = $this->getRelativePath($emsLink->getContentType());
-        $baseUrl = $this->baseUrl.$path.$this->phpApp;
+        $baseUrl = $this->baseUrl.$this->phpApp;
 
         if (\strlen($baseUrl) > 0 && 0 === \strpos($url, $baseUrl)) {
             return $url;
         }
 
         return $baseUrl.$url;
-    }
-
-    private function getRelativePath(string $contentType): string
-    {
-        foreach ($this->relativePaths as $value) {
-            if (\preg_match($value['regex'], $contentType)) {
-                return $value['path'];
-            }
-        }
-
-        return '';
     }
 }
