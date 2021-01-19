fb3251229f8556d5a7f3e1edd9c3a9cc1f3b67f8
David Mattei <david.mattei@smals.be>
2021-01-19 22:32:27 +0100

    feat: local development
    - Add new config 'local' default false. Enable in dev
    - Add PullCommand
    - Add PullHelper
    - PullHelper dump translations

---
+++ src/Command/Local/PullCommand.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EMS\ClientHelperBundle\Command\Local;
+
+use EMS\ClientHelperBundle\Helper\Environment\EnvironmentHelper;
+use EMS\ClientHelperBundle\Helper\Local\PullHelper;
+use EMS\CommonBundle\Command\CommandInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+final class PullCommand extends Command implements CommandInterface
+{
+    private PullHelper $pullHelper;
+    private EnvironmentHelper $environmentHelper;
+    private SymfonyStyle $style;
+
+    protected static $defaultName = 'emsch:local:pull';
+
+    private const OPTION_EMSCH_ENV = 'emsch_env';
+
+    public function __construct(PullHelper $pullService, EnvironmentHelper $environmentHelper)
+    {
+        parent::__construct();
+        $this->pullHelper = $pullService;
+        $this->environmentHelper = $environmentHelper;
+    }
+
+    protected function configure(): void
+    {
+        $this->addOption(self::OPTION_EMSCH_ENV, null, InputArgument::OPTIONAL, 'emsch env name', null);
+    }
+
+    protected function initialize(InputInterface $input, OutputInterface $output): void
+    {
+        $this->style = new SymfonyStyle($input, $output);
+
+        if (null === $input->getOption(self::OPTION_EMSCH_ENV)) {
+            $input->setOption(self::OPTION_EMSCH_ENV, $this->environmentHelper->getEmschEnv());
+        }
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->style->title('Sync - pull');
+
+        $environmentName = \strval($input->getOption(self::OPTION_EMSCH_ENV));
+
+        if (null === $environment = $this->environmentHelper->getEnvironment($environmentName)) {
+            throw new \RuntimeException(\sprintf('Environment with the name "%s" not found!', $environmentName));
+        }
+
+        $this->pullHelper->pullTranslations($environment);
+
+        return 1;
+    }
+}
---
+++ src/Helper/Local/PullHelper.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EMS\ClientHelperBundle\Helper\Local;
+
+use EMS\ClientHelperBundle\Helper\Environment\Environment;
+use EMS\ClientHelperBundle\Helper\Translation\TranslationBuilder;
+use Symfony\Component\Translation\Dumper\YamlFileDumper;
+
+final class PullHelper
+{
+    private TranslationBuilder $translationBuilder;
+    private string $projectDir;
+
+    public function __construct(TranslationBuilder $translationBuilder, string $projectDir)
+    {
+        $this->translationBuilder = $translationBuilder;
+        $this->projectDir = $projectDir;
+    }
+
+    public function pullTranslations(Environment $environment): void
+    {
+        $dumper = new YamlFileDumper('yaml');
+        $path = $this->getPath($environment, 'translations');
+
+        foreach ($this->translationBuilder->buildMessageCatalogues() as $messageCatalogue) {
+            $dumper->dump($messageCatalogue, ['path' => $path, 'as_tree' => true, 'inline' => 5]);
+        }
+    }
+
+    private function getPath(Environment $environment, string $folder): string
+    {
+        return \implode(DIRECTORY_SEPARATOR, [$this->projectDir, 'local', $environment->getName(), $folder]);
+    }
+}
