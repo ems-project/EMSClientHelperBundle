ffed2fd94fd3bf57f544d39699e99ba480d28656
David Mattei <david.mattei@smals.be>
2021-01-20 09:06:09 +0100

    fix: build route and translations for environment

--- src/Helper/Routing/Router.php
+++ src/Helper/Routing/Router.php
@@ -4,19 +4,26 @@ declare(strict_types=1);
 
 namespace EMS\ClientHelperBundle\Helper\Routing;
 
+use EMS\ClientHelperBundle\Helper\Environment\EnvironmentHelper;
 use Symfony\Component\Routing\RouteCollection;
 
 final class Router extends BaseRouter
 {
+    private EnvironmentHelper $environmentHelper;
     private RoutingBuilder $routeBuilder;
 
-    public function __construct(RoutingBuilder $routeBuilder)
+    public function __construct(EnvironmentHelper $environmentHelper, RoutingBuilder $routeBuilder)
     {
+        $this->environmentHelper = $environmentHelper;
         $this->routeBuilder = $routeBuilder;
     }
 
     public function getRouteCollection(): RouteCollection
     {
-        return $this->routeBuilder->buildRouteCollection();
+        if (null === $environment = $this->environmentHelper->getCurrentEnvironment()) {
+            return new RouteCollection();
+        }
+
+        return $this->routeBuilder->buildRouteCollection($environment);
     }
 }
