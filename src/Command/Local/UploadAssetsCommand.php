c48215647a70e93812275a16b96fe01096cda7ad
Mathieu De Keyzer <me@theus.be>
2021-05-27 18:13:12 +0200

    feat: asset archive upload

--- src/Command/Local/UploadAssetsCommand.php
+++ src/Command/Local/UploadAssetsCommand.php
@@ -41,13 +41,56 @@ final class UploadAssetsCommand extends AbstractLocalCommand
 
             return -1;
         }
-        $this->uploadArchive($assetsArchive);
+        $hash = $this->coreApi->hashFile($assetsArchive);
+        $filesize = \filesize($assetsArchive);
+        if (!\is_int($filesize)) {
+            throw new \RuntimeException('Unexpected file size type');
+        }
+
+        $fromByte = $this->coreApi->initUpload($hash, $filesize, 'bundle.zip', 'application/zip');
+        if ($fromByte < 0) {
+            throw new \RuntimeException(\sprintf('Unexpected negative offset: %d', $fromByte));
+        }
+        if ($fromByte > $filesize) {
+            throw new \RuntimeException(\sprintf('Unexpected bigger offset than the filesize: %d > %d', $fromByte, $filesize));
+        }
+
+        $handle = \fopen($assetsArchive, 'r');
+        if (false === $handle) {
+            throw new \RuntimeException(\sprintf('Unexpected error while open the archive %s', $assetsArchive));
+        }
+        if ($fromByte > 0) {
+            if (0 !== \fseek($handle, $fromByte)) {
+                throw new \RuntimeException(\sprintf('Unexpected error while seeking the file pointer at position %s', $fromByte));
+            }
+        }
+
+        if ($fromByte === $filesize) {
+            $this->io->comment(\sprintf('The assets %s were already uploaded', $hash));
+
+            return 1;
+        }
+
+        $this->io->progressStart($filesize);
+        $uploaded = $fromByte;
+        while (!\feof($handle)) {
+            $chunk = \fread($handle, 819200);
+            if (!\is_string($chunk)) {
+                throw new \RuntimeException('Unexpected chunk type');
+            }
+            $uploaded = $this->coreApi->addChunk($hash, $chunk);
+            $this->io->progressAdvance(\strlen($chunk));
+        }
+        $this->io->progressFinish();
+
+        if ($uploaded !== $filesize) {
+            $this->io->warning(\sprintf('Sizes mismatched %d vs. %d for assets %s', $uploaded, $filesize, $hash));
+
+            return 1;
+        }
+
+        $this->io->success(\sprintf('Assets %s have been uploaded', $hash));
 
         return 1;
-    }
-
-    protected function uploadArchive(string $assetsArchive): void
-    {
-        $this->io->success(\sprintf('Assets have been uploaded with the hash %s', $assetsArchive));
     }
 }
