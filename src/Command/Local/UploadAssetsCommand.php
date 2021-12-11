0ca959552181d8c3dfe249589f674223eb2b5d5a
Mathieu De Keyzer <me@theus.be>
2021-12-11 13:10:13 +0100

    fix: upload asets in the local asset folder (if defined) (#309)

--- src/Command/Local/UploadAssetsCommand.php
+++ src/Command/Local/UploadAssetsCommand.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace EMS\ClientHelperBundle\Command\Local;
 
+use EMS\ClientHelperBundle\Helper\Environment\EnvironmentHelper;
+use EMS\ClientHelperBundle\Helper\Local\LocalHelper;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -11,6 +13,13 @@ use Symfony\Component\Console\Output\OutputInterface;
 final class UploadAssetsCommand extends AbstractUploadCommand
 {
     private const ARG_BASE_URL = 'base_url';
+    private ?string $assetLocalFolder;
+
+    public function __construct(EnvironmentHelper $environmentHelper, LocalHelper $localHelper, ?string $assetLocalFolder)
+    {
+        parent::__construct($environmentHelper, $localHelper);
+        $this->assetLocalFolder = $assetLocalFolder;
+    }
 
     protected function configure(): void
     {
@@ -24,6 +33,9 @@ final class UploadAssetsCommand extends AbstractUploadCommand
     {
         $this->io->title('Local development - Upload assets');
         $baseUrl = $input->getArgument(self::ARG_BASE_URL);
+        if (!\is_string($baseUrl)) {
+            $baseUrl = $this->assetLocalFolder;
+        }
         if (!\is_string($baseUrl)) {
             $baseUrl = $this->environment->getAlias();
         }
